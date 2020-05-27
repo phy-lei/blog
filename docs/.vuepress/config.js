3159457c842c059a7f4fb43acdb5bdda31951dd7
phy-lei <765373325@qq.com>
2020-05-27 18:42:05 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,6 +14,14 @@ module.exports = {
         "href": "/favicon3.ico"
       }
     ],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     [
       "meta",
       {
@@ -91,6 +99,10 @@ module.exports = {
   "plugins":{
     '@vuepress/google-analytics':{
       'ga': secret.ga
+    },
+    '@vuepress/pwa': {
+      serviceWorker: true,
+      updatePopup: true
     }
   },
   "define": {
