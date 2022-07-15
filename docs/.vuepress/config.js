caa02f4aecfd5b7454d6c60f3b8ed592ebb9f2b7
phy-lei <765373325@qq.com>
2022-07-15 09:42:26 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -123,6 +123,11 @@ module.exports = {
           enable: true
         }
       }
+    },
+    keyPage: {
+      keys: ['8b4dccc83d17a858467b0732d63a0856'], // 1.3.0 版本后需要设置为密文
+      color: '#42b983', // 登录页动画球的颜色
+      lineColor: '#42b983' // 登录页动画线的颜色
     }
   },
   markdown: {
